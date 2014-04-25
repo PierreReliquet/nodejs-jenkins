e3200662375d725dc7b58b2c82ea0ac2ed3897b4
Pierre Reliquet <pierre.reliquet@gmail.com>
2014-04-25 16:35:39 +0200

    Adding some tests to have something to run with jenkins

---
+++ test.js
@@ -0,0 +1,13 @@
+var chai = require('chai');
+
+describe('a first test to check the integration with jenkins', function() {
+
+    it('should be always true', function() {
+        chai.assert.equal(true, true);
+    });
+
+    it('should be also always true', function() {
+        chai.expect(true).to.equal(true);
+    });
+
+});
